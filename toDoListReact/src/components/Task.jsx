4e227815fe63692dd033cc4dc9279c958695b9bb
Lihsolive <liliamoliveira13@gmail.com>
2023-09-20 22:31:58 -0300

    :sparkles: feat: removing tasks

--- toDoListReact/src/components/Task.jsx
+++ toDoListReact/src/components/Task.jsx
@@ -1,11 +1,21 @@
 import Checkbox from "./Checkbox";
 import PropTypes from 'prop-types';
+import trashIcon from "../assets/icons/trash.svg"
+import penIcon from "../assets/icons/pen.svg"
 
-export default function Task({name,done, onToggle}) {
+export default function Task({name,done, onToggle, onTrash}) {
   return (
     <div className={"task" + (done ? ' done' : '')}>
       <Checkbox checked={done} onClick={() => onToggle(!done)} />
+      <div className="task-name">
       <span>{name}</span>
+      </div>
+      <button className="trash-icon" onClick={onTrash}>
+        <img src={trashIcon} />
+      </button>
+      <button className="pen-icon">
+        <img src={penIcon} />
+      </button>
     </div>
   );
 }
@@ -13,5 +23,6 @@ export default function Task({name,done, onToggle}) {
 Task.propTypes = {
   name: PropTypes.string.isRequired,
   done: PropTypes.bool.isRequired,
-  onToggle: PropTypes.func.is
+  onToggle: PropTypes.func.is,
+  onTrash: PropTypes.func.is
 };
